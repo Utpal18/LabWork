af4e1605d3a3323dc7579c806458547ec1e67c4b
Utpal18 <utpalt2002@gmail.com>
2021-02-23 09:50:37 +0545

    Initial commit

---
+++ Lab1/Addition.py
@@ -0,0 +1,7 @@
+##1.WAP to input 3 different numbers in different lines and print their sum.
+
+a=int(input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+c=int(input("Enter 3rd number: "))
+sum=a+b+c
+print("The sum is: ",sum)
---
+++ Lab1/AreaOfTriangle.py
@@ -0,0 +1,9 @@
+##2.WAP that reads the length of the base and the height of a right angled triangle and prints the area.
+
+length=int(input("Enter the length of the base: "))
+height=int(input("Enter the height of the triangle: "))
+area=(1/2)*length*height
+
+print(f'The area of the triangle is {area}')
+##print("The area of the triangle is ",area )
+
