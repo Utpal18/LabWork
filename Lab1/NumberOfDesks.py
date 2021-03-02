caaeadb1d51aa0cf5f1edcfa9c828ce1aca5a4a9
Utpal18 <utpalt2002@gmail.com>
2021-03-02 10:17:31 +0545

    Number of desks

---
+++ Lab1/NumberOfDesks.py
@@ -0,0 +1,13 @@
+'''
+School decided to replace desks in 3 classrooms.Each desk sits 2 students.Given the number of students in each class,
+print the smallest number of desk that can be purchased.
+'''
+
+class1=int(input("Enter the number of students in 1st class: "))
+class2=int(input("Enter the number of students in 2nd  class: "))
+class3=int(input("Enter the number of students in 3rd class: "))
+
+total_students=(class1+class2+class3)
+desk=total_students/2
+number=round(desk)
+print("The total number of desk required is :",number)
