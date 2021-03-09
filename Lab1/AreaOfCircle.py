f949c7444c80c12d80c792320a2e599c8945abde
Utpal18 <utpalt2002@gmail.com>
2021-03-09 10:14:15 +0545

    Area of Circle

---
+++ Lab1/AreaOfCircle.py
@@ -0,0 +1,6 @@
+#WAP which accepts the radius of a circle and print the area
+
+radius=float(input("Enter the radius in cm:"))
+pi=22/7
+AreaOfCircle=pi*(radius**2)
+print(f'The area of the circle is: {AreaOfCircle} cm² ')
