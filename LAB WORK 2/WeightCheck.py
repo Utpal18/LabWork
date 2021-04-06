6920fe3b6e5c497ff0a5a265f7aa9ae96b7fec3b
Utpal18 <utpalt2002@gmail.com>
2021-04-06 09:57:28 +0545

    Weight Convertor

---
+++ LAB WORK 2/WeightCheck.py
@@ -0,0 +1,10 @@
+weight=int(input("Enter your weight: "))
+SIUnit=input("Is the weight in kg or lbs? ")
+if SIUnit=="kg":
+    new_weight=weight*2.2
+    print(f'Your weight in pound is {new_weight}')
+elif SIUnit=="lbs":
+    new_weight=weight/2.2
+    print(f'Your weight in kilogram is {new_weight}')
+else:
+    print("No Valid Input")
