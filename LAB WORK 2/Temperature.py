64c53239ba00cf3eb4430c88b5e944c785abb65f
Utpal18 <utpalt2002@gmail.com>
2021-04-06 09:37:27 +0545

    Temperature Check

---
+++ LAB WORK 2/Temperature.py
@@ -0,0 +1,8 @@
+temp=float(input("Enter the temperature: "))
+if temp>=30:
+    print("Today is a hot day.")
+elif temp<=10:
+    print("Today is a cold day.")
+else:
+    print("Today is neither cold nor hot day.")
+
