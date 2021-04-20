fb0148b48d3547eceb7ccad358a37bef41ea5460
Utpal18 <utpalt2002@gmail.com>
2021-04-20 09:21:47 +0545

    Positive Negetive

---
+++ LAB WORK 2/positive neg.py
@@ -0,0 +1,11 @@
+num=int(input('enter num'))
+positive=False
+if num>0:
+    positive=True
+    print(positive)
+elif num<0:
+    positive=False
+    print(positive)
+
+else:
+    print('number is zero')
